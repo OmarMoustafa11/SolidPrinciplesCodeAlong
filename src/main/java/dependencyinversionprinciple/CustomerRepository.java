24b3f676672a2e8eaa521f684df6938dfe86d29a
omarmoustafa11 <omaroj99@yahoo.com>
2022-10-17 22:14:47 -0400

    code along

---
+++ src/main/java/dependencyinversionprinciple/CustomerRepository.java
@@ -0,0 +1,10 @@
+package dependencyinversionprinciple;
+
+import java.util.List;
+
+public interface CustomerRepository {
+    List<String> findNames();
+
+
+
+}
---
+++ src/main/java/dependencyinversionprinciple/CustomerService.java
@@ -0,0 +1,11 @@
+package dependencyinversionprinciple;
+
+public class CustomerService {
+    private final CustomerRepository customerDao;
+
+    public CustomerService(CustomerRepository customerDao) {
+        this.customerDao = customerDao;
+    }
+
+
+}
